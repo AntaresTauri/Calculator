2d4daccaff71fce85bb321384be0cc95b255bd2f
Antares <antarestauri@gmail.com>
2023-12-29 16:34:46 +0300

    The screen isn't loading

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,16 +10,11 @@ android {
     defaultConfig {
         applicationId = "com.antares.calculator"
         minSdk = 29
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
-
-    // Подключаем ViewBinding
-    buildFeatures {
-        viewBinding = true
     }
 
     buildTypes {
@@ -46,6 +41,10 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.6")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.6")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
